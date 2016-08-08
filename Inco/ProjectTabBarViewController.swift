b420a667b5e4637b5c4d4bb076f308b020584c84
Kien Pham <kienbk1910@gmail.com>
2016-08-08 17:59:23 +0700

    new ticket

--- Inco/ProjectTabBarViewController.swift
+++ Inco/ProjectTabBarViewController.swift
@@ -13,21 +13,33 @@ class ProjectTabBarViewController: UITabBarController {
     var detail:UIBarButtonItem? = nil
     var addTicketBtn:UIBarButtonItem? = nil
     var item:ProjectCell?
+    var taskview:MainViewController?
+    var ticketView:MainViewController?
+    var discussionView:MainViewController?
     override func viewDidLoad() {
         super.viewDidLoad()
-        refresh = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Refresh,target:self,action:nil)
+        refresh = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Refresh,target:self,action:#selector(ProjectTabBarViewController.refreshView))
         detail = UIBarButtonItem(image: UIImage(named: "ic_description"), style: .Plain, target: self, action: #selector(ProjectTabBarViewController.showDetailProject))
         addTicketBtn = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Add,target:self,action:#selector(ProjectTabBarViewController.addTicket));
      //   detail = UIBarButtonItem(title: "Detail", style: .Plain, target: self, action: nil)
      //   detail.
-        navigationItem.rightBarButtonItems = [refresh!,addTicketBtn!,detail!]
+        navigationItem.rightBarButtonItems = [refresh!,detail!]
 
         // Do any additional setup after loading the view.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    func refreshView() {
+        if self.tabBar.selectedItem?.tag == 1 {
+            self.taskview?.refresh()
+        } else if self.tabBar.selectedItem?.tag == 2{
+            self.ticketView?.refresh()
+        }else{
+            self.discussionView?.refresh()
+        }
     }
     func addTicket()  {
         let tabBarController = AddTicketTabBarViewController()
@@ -38,6 +50,10 @@ class ProjectTabBarViewController: UITabBarController {
         let file = FilesTableViewController(nibName: "CommentsTableViewController", bundle: nil)
         
         let controllers = [content,config,file]
+        tabBarController.configView = config
+        tabBarController.contentView = content
+        tabBarController.fileView = file
+        tabBarController.projectID = (self.item?.id)!
         tabBarController.viewControllers = controllers
         let firstImage = UIImage(named: "tabs_ticket")
         let secondImage = UIImage(named: "ic_settings_white")
@@ -70,6 +86,7 @@ class ProjectTabBarViewController: UITabBarController {
         tabBarController.iD = (self.item?.id)!
         tabBarController.projectID = (self.item?.id)!
         let controllers = [detail,comment]
+        tabBarController.commentView = comment
         tabBarController.viewControllers = controllers
         let firstImage = UIImage(named: "ic_description")
         let secondImage = UIImage(named: "tabs_discusstion")
@@ -85,7 +102,13 @@ class ProjectTabBarViewController: UITabBarController {
         //tabBarController.title = (self.projects[indexPath.row] ).name
 
     }
-
+    override func tabBar(tabBar: UITabBar, didSelectItem item: UITabBarItem) {
+        if item.tag == 2 {
+            navigationItem.rightBarButtonItems = [refresh!,addTicketBtn!,detail!]
+        }else{
+            navigationItem.rightBarButtonItems = [refresh!,detail!]
+        }
+    }
     /*
     // MARK: - Navigation
 
