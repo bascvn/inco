8a5baf7a0138a94a68a60817570d1033896d14be
Kien Pham <kienbk1910@gmail.com>
2016-08-11 22:20:06 +0700

    NEW_COMMENT

--- Inco/TicketTableViewCell.swift
+++ Inco/TicketTableViewCell.swift
@@ -24,7 +24,7 @@ class TicketTableViewCell: UITableViewCell {
 
     override func setSelected(selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
     
