708594cf306d7640c0000317bd1c90e4224de03d
Kien Pham <kienbk1910@gmail.com>
2016-08-12 06:56:33 +0700

    sds

---
+++ Inco/RefreshProtocol.swift
@@ -0,0 +1,12 @@
+//
+//  RefreshProtocol.swift
+//  Inco
+//
+//  Created by admin on 7/31/16.
+//  Copyright © 2016 Ban Sac. All rights reserved.
+//
+
+import Foundation
+protocol RefreshProtocol {
+    func refresh()
+}
